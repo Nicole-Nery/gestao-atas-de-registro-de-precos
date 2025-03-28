06be402475ab2a72921e395e74d1130c042d1ff3
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-03-28 18:08:16 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 
+# Alterando o nome da página e o ícone
+st.set_page_config(page_title="Gestão de ARP", page_icon="📄")
+st.title("Sistema de Gestão de Atas de Registro de Preços")
+st.write("Bem-vindo ao sistema de controle de atas, onde você pode gerenciar saldos, acompanhar validade e gerar relatórios.")
+
 # Inicializa session state para armazenar dados temporários
 if "atas" not in st.session_state:
     st.session_state.atas = []
