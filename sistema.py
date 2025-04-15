a79a6e23c824a141568c8f6edbe4023791f2ac51
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-04-15 20:54:17 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -48,7 +48,7 @@ with tabs[0]:
             .stButton > button {
                 border-radius: 0;
                 background-color: #f8f8f8;
-                color: #003366;
+                color: #003366 !important;
                 box-shadow:0 4px 8px rgba(0,0,0,0.1);
                 padding: 1rem 1rem;
                 margin-bottom: 0.5rem;
