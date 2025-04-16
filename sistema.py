0b9369a54cb4ea899bc4f8ca0a6db2571879f787
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-04-15 21:16:21 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -4,27 +4,3 @@ html, body, .stApp * {
     font-family: 'Rawline', sans-serif !important;
     color: #333333;
 }
-
-:root {
-        --primary-color: #1E90FF;
-        --primary-color-light: #63B3ED;
-    }
-
-    .stButton>button {
-        background-color: var(--primary-color) !important;
-        color: white !important;
-        border: none !important;
-    }
-
-    .stButton>button:hover {
-        background-color: var(--primary-color-light) !important;
-        color: white !important;
-    }
-
-    .stSlider > div[data-baseweb="slider"] {
-        color: var(--primary-color) !important;
-    }
-
-    input:checked + div:after {
-        background-color: var(--primary-color) !important;
-    }
