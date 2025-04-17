0a649709a8b021408dca060aae808c3bac99bf3f
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-04-17 12:56:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ col1, col2 = st.columns([1,5])
 with col1:
     st.image('assets/logo-sigah.png', width=300)
 with col2:
-    st.header(st.markdown("## Sistema Integrado de Gestão de Atas Hospitalares"))
+    st.header("Sistema Integrado de Gestão de Atas Hospitalares")
 st.write("Bem-vindo ao sistema de controle de atas, onde você pode gerenciar saldos, acompanhar validade das atas e visualizar relatórios.")
 
 # Estabelecendo o layout com abas
