dd87b38d53bcd652ce3edb7ae95a81346f923070
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-04-17 12:48:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.image("assets/logos.png", width=300)
 
 col1, col2 = st.columns([1,5])
 with col1:
-    st.image('assets/logo-sigah.png', width=200)
+    st.image('assets/logo-sigah.png', width=300)
 with col2:
     st.title("Sistema Integrado de Gestão de Atas Hospitalares")
 st.write("Bem-vindo ao sistema de controle de atas, onde você pode gerenciar saldos, acompanhar validade das atas e visualizar relatórios.")
