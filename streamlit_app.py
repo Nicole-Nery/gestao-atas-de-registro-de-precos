2ff3173a1791ff6be9630ea4bc8b0ae28c41b070
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-09 08:52:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ def login():
                 st.success("Login bem-sucedido! Redirecionando...")
                 st.session_state.usuario = {"email": email}
                 st.session_state["modo"] = "home"
-                st.rerun()
+                #st.rerun()
             else:
                 st.error("E-mail ou senha inválidos.")
 
