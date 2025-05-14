c57924f454b84daf85e28b04abec9367b6ab50cb
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-14 08:30:49 -0300

    Update home.py

--- home.py
+++ home.py
@@ -15,7 +15,7 @@ def show_home():
 
     usuario = st.session_state.get("usuario", {})
 
-    col1, col2 = st.columns([6, 4])
+    col1, col2 = st.columns([7, 3])
     with col1:
         st.write(f"Olá, {usuario.get('email', 'usuário')}!")
     with col2:
