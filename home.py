4fdc8e49900b0cfb1fe3f6af556749343dadc059
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-15 09:37:37 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1198,7 +1198,7 @@ def show_home():
 
                     # Adicionar à lista de renovações próximas (90 e 30 dias)
                     if dias_para_renovacao < 0:
-                        if dias_para_renovacao > -2:
+                        if dias_para_renovacao > -31:
                             renovacoes_vencidas.append(f"**Ata:** {ata['nome']} — Vencida há {-dias_para_renovacao} dia(s)")
                     elif dias_para_renovacao <= 30:
                         renovacoes_30_dias.append(f"**Ata:** {ata['nome']} — {dias_para_renovacao} dias restantes")
