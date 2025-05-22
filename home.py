6d68b6fafb6c6a884bea11542c7c57e0ddea268e
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-22 10:27:50 -0300

    Update home.py

--- home.py
+++ home.py
@@ -580,8 +580,7 @@ def show_home():
                         nova_data = st.date_input("Data da Ata", format="DD/MM/YYYY", value=pd.to_datetime(ata_info["data_inicio"]).date())
                         nova_validade_ata = st.date_input("Validade da Ata", min_value=nova_data, format="DD/MM/YYYY", value=pd.to_datetime(ata_info["data_validade"]).date())
                         novo_fornecedor_nome = st.selectbox("Fornecedor", [dicionario["nome"] for dicionario in fornecedores_nomes], key="selecione_novo_fornecedor_nome", index=[dicionario["nome"] for dicionario in fornecedores_nomes].index(nome_fornecedor_atual) if nome_fornecedor_atual else 0)
-
-                        nova_categoria_ata = st.selectbox("Categoria", ["Equipamentos médicos", "Infraestrutura hospitalar", "Suprimentos"], key="selecione_nova_categoria_ata", value="Infraestrutura hospitalar")
+                        nova_categoria_ata = st.selectbox("Categoria", ["Equipamentos médicos", "Infraestrutura hospitalar", "Suprimentos"], value=ata_info["categoria_ata"], key="selecione_nova_categoria_ata")
                         novo_link_ata = st.text_input("Número do Protocolo SEI")
                         nova_info_renovacao = st.radio("Ata renovável?", options=["Sim", "Não"], horizontal=True)
                         nova_info_renovacao_bool = nova_info_renovacao == 'Sim'
