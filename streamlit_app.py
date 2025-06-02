9981ae6b1319c9a3e937916fe328ec259ee8c3f1
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-06-02 14:53:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,4 @@ with open(caminho_css) as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-
-# Fluxo - Login ou cadastro
-if "usuario" not in st.session_state:
-    mostrar_tela_login_ou_cadastro()
-    st.stop()
+mostrar_tela_login_ou_cadastro()
