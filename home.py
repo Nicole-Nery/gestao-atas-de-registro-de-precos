56d5633def43c68c98cc3eacc810c126a5fac983
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-06-04 10:47:41 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1354,8 +1354,8 @@ def show_home():
 
 
     # Renovação da Ata -----------------------------------------------------------------------------------------------------------------------------
-    resposta = supabase.table('configuracoes').select("valor").eq("chave", "prazo_renovacao_ata").execute()
-    prazo_renovacao_ata = resposta.data[0]["valor"]
+    resposta = supabase.table('configuracoes').select("valor").eq("chave", "prazo_renovacao_ata").single().execute()
+    prazo_renovacao_ata = resposta.data["valor"]
 
     if 'prazo_renovacao_ata' not in st.session_state:
         st.session_state.prazo_renovacao_ata = prazo_renovacao_ata
