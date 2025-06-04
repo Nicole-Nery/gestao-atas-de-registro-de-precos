df620b1b2f1023ea192b6eeb2fcf7382d0fa020a
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-06-04 10:29:03 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from supabase import create_client, Client
 
 # Conectar ao Supabase
-SUPABASE_URL = "https://btstungeitzcizcysupd.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0c3R1bmdlaXR6Y2l6Y3lzdXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwNjAxNTUsImV4cCI6MjA1OTYzNjE1NX0.L1KZfGO_9Cq7iOGtdDVD4bGp02955s65fjcK2I1jntc"
+SUPABASE_URL = "https://zfqhryctnzysdndihfjg.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmcWhyeWN0bnp5c2RuZGloZmpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNDI3MjEsImV4cCI6MjA2NDYxODcyMX0.vv1v29D0TOo7RfSBcJxKCTs3swFGNj6lSQ0eDDvRDcA"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
